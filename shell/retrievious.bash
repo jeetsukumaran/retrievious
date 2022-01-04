c52ebe25da74eed5a54118efc410777ae72fa00e
Jeet Sukumaran <jeetsukumaran@gmail.com>
2022-01-04 11:28:31 -0800

    Update

--- shell/retrievious.bash
+++ shell/retrievious.bash
@@ -80,14 +80,14 @@ then
             local exc=${_FZF_FDFIND_EXCLUDE}
         fi
         # eval "${RETRIEVIOUS_FDFIND_PATH} . --type f ${exc} ${RETRIEVIOUS_FDFIND_OPTS} ${1}"
-        # `$(tr '${RETRIEVIOUS_GLOBAL_PATH_SEPARATOR}' ' ' <<< ${1})`: split colon delimited string into space delimited strings
-        eval "${RETRIEVIOUS_FDFIND_PATH} . --type f ${exc} ${RETRIEVIOUS_FDFIND_OPTS} $(tr ':' ' ' <<< ${1})"
+        # `$(tr '${RETRIEVIOUS_GLOBAL_PATH_SEPARATOR}' ' ' <<< ${1})`: split semicolon delimited string into space delimited strings
+        eval "${RETRIEVIOUS_FDFIND_PATH} . --type f ${exc} ${RETRIEVIOUS_FDFIND_OPTS} $(tr ';' ' ' <<< ${1})"
     }
 
     function __f_find_dir__() {
         # eval "${RETRIEVIOUS_FDFIND_PATH} . --type d ${_FZF_FDFIND_EDIT_EXCLUDE} ${RETRIEVIOUS_FDFIND_OPTS} ${1}"
         # `$(tr '${RETRIEVIOUS_GLOBAL_PATH_SEPARATOR}' ' ' <<< ${1})`: split colon delimited string into space delimited strings
-        eval "${RETRIEVIOUS_FDFIND_PATH} . --type d ${_FZF_FDFIND_EDIT_EXCLUDE} ${RETRIEVIOUS_FDFIND_OPTS} $(tr ':' ' ' <<< ${1})"
+        eval "${RETRIEVIOUS_FDFIND_PATH} . --type d ${_FZF_FDFIND_EDIT_EXCLUDE} ${RETRIEVIOUS_FDFIND_OPTS} $(tr ';' ' ' <<< ${1})"
     }
 else
     function __f_compose_fzf_find_prune__() {
